d793bbeb747d4f492944e7330d2feae5eac710aa
Rajkumarpatil307 <patil.rajkumar307@gmail.com>
2024-08-28 18:14:22 +0530

    UC1 file was commited

--- AdressBook.java
+++ AdressBook.java
@@ -1,9 +1,19 @@
 package com.AdressBookSysem;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class AdressBook {
 
 	public static void main(String[] args) {
      System.out.println("Welcome to Adress Book");
+     Scanner s=new Scanner(System.in);
+     List<Contacts> cl=new ArrayList<Contacts>();
+     System.out.println("want to add the contact person to Adress Book?");
+     if(s.next()=="yes") {
+    	Contacts c=new Contacts();
+     }
 	}
-
-}
+     
+	}
